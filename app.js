a76be5a065b2696cf275f06ff44992201d22a863
Peter Soboyejo <thepcmrtim@gmail.com>
2016-02-23 17:18:46 -0500

    Some stuff I might need

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express'),
-app = express();
+app = express(),
+fs = require('fs'),
+twilio = require('twilio'),
+cheerio = require('cheerio'),
+md5 = require('md5');
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server is listening on port %d in %s mode", this.address().port, app.settings.env);
