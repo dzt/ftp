07d324e4a01853228d78d921ef6946e908856df8
Peter Soboyejo <thepcmrtim@gmail.com>
2016-04-02 15:00:31 -0400

    Push

--- android/app/src/main/java/com/kcufthepopulation/android/MainActivity.java
+++ android/app/src/main/java/com/kcufthepopulation/android/MainActivity.java
@@ -3,11 +3,16 @@ package com.kcufthepopulation.android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.onesignal.OneSignal;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OneSignal.startInit(this).init();
+
     }
 }
